331fdbfba8231bbc43d22716c6e773162af807d2
MSP-Greg <Greg.mpls@gmail.com>
2021-04-26 17:28:12 -0500

    Change Windows MSYS package location (no BinTray)

--- open_knapsack_pkgs.js
+++ open_knapsack_pkgs.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const baseUri = 'https://dl.bintray.com/oneclick/OpenKnapsack/x64'
+const baseUri = 'https://github.com/MSP-Greg/ruby-msys2-package-archive/releases/download/ri-msys-pkgs'
 
 const baseSuf64 = 'x64-windows.tar.lzma'
 
 export const old_pkgs = {
+  'gdbm'    : `${baseUri}/gdbm-1.8.3-${baseSuf64}`,
   'libffi'  : `${baseUri}/libffi-3.2.1-${baseSuf64}`,
+  'libiconv': `${baseUri}/libiconv-1.14-${baseSuf64}`,
+  'libiyaml': `${baseUri}/libyaml-0.1.7-${baseSuf64}`,
   'openssl' : `${baseUri}/openssl-1.0.2j-${baseSuf64}`,
   'ragel'   : `${baseUri}/ragel-6.7-${baseSuf64}`,
   'sqlite3' : `${baseUri}/sqlite-3.7.15.2-${baseSuf64}`,
